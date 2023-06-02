54fe59c29b8ff2b9a8d5407f16a20d82c2100807
Ebram Sobhy <116510911+Ebramsobhy@users.noreply.github.com>
2023-06-02 20:40:42 +0300

    Add files via upload

---
+++ Rails/lab1/db/migrate/20230530012118_tracks.rb
@@ -0,0 +1,9 @@
+class Tracks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tracks do |t|
+      t.string :name, unique: true
+      t.text :description, null: true
+      t.string :logo
+      t.timestamps
+  end
+end
